1fa2495bb494b3af65133f8fb869f61e9fd71154
ameni97 <amani.methnani@esprit.tn>
2023-01-19 09:50:52 +0100

    updated

--- tests/test_atom_generics.py
+++ tests/test_atom_generics.py
@@ -1,1 +1,31 @@
 # Test A.t and A[int].t and A[int] is A[int] -> True
+import pytest 
+import sys 
+from typing import Generic, TypeVar 
+
+from atom.api import (
+
+Atom,
+Int,
+Str,
+Dict,
+Value,
+Bool,
+Instance,
+
+
+
+
+)
+
+def test_generics() :
+    T = TypeVar("T", bound= object)
+
+    class A(Atom, Generic[T]):
+        t: T
+    assert isinstance(A.t, Value)
+    assert isinstance(A[int].t, Int)
+    
+
+    
+
